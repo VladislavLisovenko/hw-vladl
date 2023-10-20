ac449d7f41ad96ff3455040975beaa91bcf36a21
Vladislav L <vla78an@bk.ru>
2023-10-21 04:58:02 +0700

    hw07_word_counter is completed

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,35 @@
-package main
+package hw07wordcounter
 
-func main() {
-	// Place your code here.
+import "strings"
+
+func extraSymbols() []string {
+	return []string{
+		",",
+		".",
+		"!",
+		"?",
+		":",
+		";",
+		"-",
+		"\t",
+		"\n",
+	}
+}
+
+func countWords(text string) map[string]int {
+	m := map[string]int{}
+
+	for _, s := range extraSymbols() {
+		text = strings.ReplaceAll(text, s, " ")
+	}
+
+	words := strings.Split(text, " ")
+	for _, word := range words {
+		if word == "" {
+			continue
+		}
+		m[word]++
+	}
+
+	return m
 }
---
+++ hw07_word_counter/main_test.go
@@ -0,0 +1,53 @@
+package hw07wordcounter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func tests() []struct {
+	descr    string
+	text     string
+	expected map[string]int
+} {
+	return []struct {
+		descr    string
+		text     string
+		expected map[string]int
+	}{
+		{
+			descr:    "empty string",
+			text:     "",
+			expected: map[string]int{},
+		},
+		{
+			descr: "two different words",
+			text:  "one?, \n two!\n",
+			expected: map[string]int{
+				"one": 1,
+				"two": 1,
+			},
+		},
+		{
+			descr: "two repeated words",
+			text:  "one, 	two?.\n    one-two. one",
+			expected: map[string]int{
+				"one": 3,
+				"two": 2,
+			},
+		},
+	}
+}
+
+func TestCountWords(t *testing.T) {
+	tests := tests()
+
+	for _, td := range tests {
+		td := td
+		t.Run(td.descr, func(t *testing.T) {
+			got := countWords(td.text)
+			require.Equal(t, td.expected, got)
+		})
+	}
+}
