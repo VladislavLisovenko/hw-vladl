25b282dbe84ea32c64095182c69d95b2f1ba5dbd
Vladislav L <vla78an@bk.ru>
2023-11-02 17:05:39 +0700

    comments are fixed

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,60 +1,88 @@
 package main
 
 import (
-	"context"
+	"crypto/rand"
 	"fmt"
+	"math/big"
 	"time"
 )
 
-func Sensor(ctx context.Context, ch chan<- float64) {
-	i := 0.0
-	stop := false
-	for !stop {
-		i += 1.0
-		select {
-		case ch <- i:
-		case <-ctx.Done():
-			stop = true
-		}
-		time.Sleep(100 * time.Millisecond)
+func randFloat64() float64 {
+	max := big.NewInt(1_000_000_000)
+	randInt, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		panic(err)
 	}
+	return float64(randInt.Int64())
 }
 
-func SensorReader(ctx context.Context, ch <-chan float64, processedData chan<- float64) {
-	sum := 0.0
-	v := 0.0
-	i := 0
-	for {
-		i++
-		select {
-		case v = <-ch:
-			sum += v
-			if i%10 == 0 {
-				processedData <- (sum / 10)
-				sum = 0.0
+func Sensor(d time.Duration) chan float64 {
+	// создаем канал
+	res := make(chan float64)
+	// запускаем горутину
+	go func() {
+		// закроем канал перед выходом из функции
+		defer close(res)
+		// создаем таймер
+		// After - помещает значение в канал по прошествии заданного временни
+		timer := time.After(d)
+		// бесконечный цикл
+		for {
+			select {
+			// помещаем в канал очередное значение
+			case res <- randFloat64():
+			// как только значение таймера прочитано - завершаем выполнение функции
+			case <-timer:
+				return
 			}
-		case <-ctx.Done():
-			return
+			// пауза 100 миллисекунд
+			time.Sleep(100 * time.Millisecond)
 		}
-	}
+	}()
+	// возвращаем канал
+	return res
+}
+
+// в параметре sensorData ReadOnly-канал.
+func SensorReader(sensorData <-chan float64) chan float64 {
+	// создаем канал
+	res := make(chan float64)
+
+	// запускаем горутину
+	go func() {
+		// закроем канал перед выходом из функции
+		defer close(res)
+		// инициализация счетчика итераций цикла, по которому будем определять каждое 10-е значение
+		i := 0
+		// инициализация переменной для суммирования получаемых из сенсора данных
+		var accum float64
+		// получаем даные из сенсора
+		for v := range sensorData {
+			// инкремент счетчика
+			i++
+			// прибавляем очередное значение, полученное из сенсора
+			accum += v
+			// если получено 10-е значение, то вычисляем среднее значение,
+			// помещаем его в результирующий канал и обнуляем переменную суммы
+			if i%10 == 0 {
+				res <- accum / 10.0
+				accum = 0.0
+			}
+		}
+	}()
+
+	// возвращаем канал
+	return res
 }
 
 func main() {
-	sensorData := make(chan float64)
-	processedData := make(chan float64)
+	// запускаем счетчик
+	dataCh := Sensor(time.Minute)
+	// передаем ссылку на канал сенсора в ридер и получаем ссылку на канал со средними значениями
+	acumCh := SensorReader(dataCh)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	go Sensor(ctx, sensorData)
-	go SensorReader(ctx, sensorData, processedData)
-
-	for {
-		select {
-		case pd := <-processedData:
-			fmt.Printf("%f\n", pd)
-		case <-ctx.Done():
-			return
-		}
+	// выводим на экран значения из канала со средними значениями
+	for d := range acumCh {
+		fmt.Println(d)
 	}
 }
--- hw10_motion_sensor/main_test.go
+++ hw10_motion_sensor/main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -32,28 +32,41 @@ func sensorTestData() []struct {
 			descr:    "4th iteration",
 			expected: 35.5,
 		},
+		{
+			descr:    "5th iteration",
+			expected: 45.5,
+		},
+	}
+}
+
+func DataGenerator() chan float64 {
+	res := make(chan float64)
+	go func() {
+		defer close(res)
+		for i := 1; i <= 50; i++ {
+			res <- float64(i)
+		}
+	}()
+	return res
+}
+
+func TestSensor(t *testing.T) {
+	sensorDataCh := Sensor(time.Second * 5)
+	var float64Value float64
+	for v := range sensorDataCh {
+		fmt.Println(v)
+		require.IsType(t, float64Value, v)
 	}
 }
 
 func TestSensorReader(t *testing.T) {
-	sensorData := make(chan float64)
-	processedData := make(chan float64)
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	go Sensor(ctx, sensorData)
-	go SensorReader(ctx, sensorData, processedData)
-
-	testData := sensorTestData()
-	for _, td := range testData {
-		td := td
-		t.Run(td.descr, func(t *testing.T) {
-			select {
-			case got := <-processedData:
-				require.Equal(t, td.expected, got)
-			case <-ctx.Done():
-				return
-			}
-		})
+	tests := sensorTestData()
+	sensorCh := DataGenerator()
+	sensorReaderCh := SensorReader(sensorCh)
+	i := 0
+	for v := range sensorReaderCh {
+		fmt.Println(v)
+		require.Equal(t, tests[i].expected, v)
+		i++
 	}
 }
