29f2eb4101b210126dd5befda031ced6970ce00e
Vladislav L <vla78an@bk.ru>
2023-11-23 17:37:09 +0700

    hw11_worker_pool is completed

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func Increment(n *int, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
+	mu.Lock()
+	*n++
+	fmt.Println(*n)
+	mu.Unlock()
+}
+
 func main() {
-	// Place your code here.
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+
+	v := 0
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go Increment(&v, wg, mu)
+	}
+
+	wg.Wait()
+	fmt.Println(v)
 }
